521fcda22f22a1aeb63bebdd59c82efa638a84a4
swellisgood <swellisgood@gmail.com>
2025-02-22 20:36:37 +0100

    added dapui, zenmode and undotree plugins

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -45,9 +45,9 @@ require('lazy').setup({
 
   require 'kickstart/plugins/treesitter',
 
-  require 'custom/plugins/oil',
+  -- require 'custom/plugins/oil',
 
-  require 'custom/plugins/catppuccin',
+  -- require 'custom/plugins/catppuccin',
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
@@ -58,17 +58,17 @@ require('lazy').setup({
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
-  -- require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
-  require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.lint',
+  require 'kickstart.plugins.autopairs',
+  -- require 'kickstart.plugins.neo-tree',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
